3ec27ac164c9f066897c844b00b5e1538297b99a
Aman Kamal <109606739+KAMALAMAN687@users.noreply.github.com>
2022-07-20 02:29:06 +0530

    Add files via upload

---
+++ ddmmyy.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter DD/MM/YYYY");
+    scanf("%d%d%d",&a,&b,&c);
+    printf("%d/%d/%d",a,b,c);
+    printf("Day-%d,Month-%d,Year-%d",a,b,c);
+    return 0;
+
+}
