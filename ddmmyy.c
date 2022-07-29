b3c8c1c0b8d1a4c7dd717c5fd897c56448e5edaf
Aman Kamal <109606739+KAMALAMAN687@users.noreply.github.com>
2022-07-30 00:10:40 +0530

    Update ddmmyy.c

--- ddmmyy.c
+++ ddmmyy.c
@@ -3,7 +3,7 @@ int main()
 {
     int a,b,c;
     printf("enter DD/MM/YYYY");
-    scanf("%d%d%d",&a,&b,&c);
+    scanf("%d/%d/%d",&a,&b,&c);
     printf("%d/%d/%d",a,b,c);
     printf("Day-%d,Month-%d,Year-%d",a,b,c);
     return 0;
